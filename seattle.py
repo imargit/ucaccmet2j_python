b586c7fd7f14581df0f370bb3b9260fe31f81536
Imar Sibbing <imarsibbing@gmail.com>
2025-01-14 12:29:24 +0100

    1.2 - total_yearly and relative_monthly added, and added the figures to the repo

--- seattle.py
+++ seattle.py
@@ -13,18 +13,28 @@ for measurement in precipitation:
             seattle[measurement['date']] = measurement['value']
         else:
             seattle[measurement['date']] = seattle[measurement['date']] + measurement['value']
-#print(seattle)
 
 #1.3 Calculate the monthly precipitation for Seattle
 total_monthly_precipitation = []
 for month in range(1, 13):
     total_monthly_precipitation.append(sum([value for key, value in seattle.items() if int(key[5:7]) == month]))
 
-#1.4 Save results to a JSON file
+#2.1 Calculate the annual precipitation for Seattle
+total_yearly_precipitation = []
+total_yearly_precipitation.append(sum([value for key, value in seattle.items() if int(key[0:4]) == 2010]))
+
+#2.2 Calculate the relative monthly precipitation for Seattle
+relative_monthly_precipitation =[]
+for month in range(1, 13):
+    relative_monthly_precipitation.append(sum([value for key, value in seattle.items() if int(key[5:7]) == month])/total_yearly_precipitation[0])
+
+#1.4,2.2 Save results to a JSON file
 results = {"Seattle":
            {"station": "GHCND:US1WAKG0038",
             "state": "WA",
-            "total_monthly_precipitation": total_monthly_precipitation
+            "total_monthly_precipitation": total_monthly_precipitation,
+            "total_yearly_precipitation": total_yearly_precipitation,
+            "relative_monthly_precipitation": relative_monthly_precipitation
             }
         }
 with open('results.json','w') as file:
