2105c93ed5b47e4fe35080f22ecfde156f0fccdb
Imar Sibbing <imarsibbing@gmail.com>
2025-01-14 14:48:45 +0100

    1.3 finished, assignment complete!

--- analysis.py
+++ analysis.py
@@ -9,41 +9,48 @@ station = pd.read_csv('stations.csv',usecols=['Location','State','Station'])
 # Create a dictionary with locations as keys and station ids as values
 locations = {}
 for index, row in station.iterrows():
-    locations[row['Location']] = row['Station']
-
+    locations[row['Location']] = {
+        'station': row['Station'],
+        'state': row['State']
+    }
 results = {}
 
 for location in locations:
-    print(location)
     #1.2 Select all measurements for the city
     city = {}
     for measurement in precipitation:
-        if measurement['station'] == locations[location]:
+        if measurement['station'] == locations[location]['station']:
             if measurement['date'] not in city:
                 city[measurement['date']] = measurement['value']
             else:
                 city[measurement['date']] = city[measurement['date']] + measurement['value']
-
     #1.3 Calculate the monthly precipitation for the city
     total_monthly_precipitation = []
     for month in range(1, 13):
         total_monthly_precipitation.append(sum([value for key, value in city.items() if int(key[5:7]) == month]))
 
     #2.1 Calculate the annual precipitation for the city
-    total_yearly_precipitation = []
-    total_yearly_precipitation.append(sum([value for key, value in city.items() if int(key[0:4]) == 2010]))
+    total_yearly_precipitation = 0
+    total_yearly_precipitation = (sum([value for key, value in city.items() if int(key[0:4]) == 2010]))
     #2.2 Calculate the relative monthly precipitation for the city
     relative_monthly_precipitation =[]
     for month in range(1, 13):
-        relative_monthly_precipitation.append(sum([value for key, value in city.items() if int(key[5:7]) == month])/total_yearly_precipitation[0])
+        relative_monthly_precipitation.append(sum([value for key, value in city.items() if int(key[5:7]) == month])/total_yearly_precipitation)
 
+    #3.2 Calculate the relative yearly precipitation per city
+    total_precipitation = 0
+    for measurement in precipitation:
+        total_precipitation = total_precipitation + measurement['value']
+    relative_yearly_precipitation = total_yearly_precipitation/total_precipitation
+    
     #1.4,2.2 Save results to a JSON file
     results[location] = {
-        "station": locations[location],
-        "state": "WA",
+        "station": locations[location]['station'],
+        "state": locations[location]['state'],
         "total_monthly_precipitation": total_monthly_precipitation,
         "total_yearly_precipitation": total_yearly_precipitation,
-        "relative_monthly_precipitation": relative_monthly_precipitation
+        "relative_monthly_precipitation": relative_monthly_precipitation,
+        "relative_yearly_precipitation": relative_yearly_precipitation
         }
     with open('results.json','w') as file:
         json.dump(results,file,indent=4)
